944ed887e40a27ee024cec54a19b9ce2c17783ad
ben9955 <ben95ansari@gmail.com>
2025-01-16 17:31:09 +0000

    added GraphQL type definitions for User schema and operations

--- backend/typeDefs/user.typeDef.js
+++ backend/typeDefs/user.typeDef.js
@@ -1,10 +1,39 @@
 const userTypeDef = `#graphql 
 type User {
     _id: ID!
-    usernmae: String!
+    username: String!
     name: String!
     password: String!
     profilePicture: String
     gender: String!
 }
+
+type Query {
+    users: [User!]
+    authUser: User
+    user(userId:ID!): User
+}
+
+type Mutation {
+    singup(input: SignUpInput!): User
+    login(input:LoginInput!): User
+    logout: LogoutResponse
+}
+
+input SignUpInput{
+    username: String!
+    name: String!
+    password: String!
+    gender: String!
+}
+
+input LoginInput{
+    username: String!
+    password: String!
+}
+
+type LogoutResponse{
+    message: String!
+}
+
 `;
